a1eb32eee3eda8a7e467e39742e2152048bf1644
FaceOfBo0 <70140104+FaceOfBo0@users.noreply.github.com>
2025-02-27 14:42:13 +0100

    day 12 part 1 is correct and finished!

--- day12/part1.go
+++ day12/part1.go
@@ -7,12 +7,12 @@ import (
 	mapset "github.com/deckarep/golang-set/v2"
 )
 
-type Patch struct {
+type Region struct {
 	plantType  string
 	areas      []int
 	perimeters []int
 	overlaps   []int
-	borderSets []mapset.Set[Coordinate]
+	patches    []mapset.Set[Coordinate]
 }
 
 type Coordinate struct {
@@ -20,91 +20,121 @@ type Coordinate struct {
 	pnt utils.Point
 }
 
-func NewPatch(plantType string) *Patch {
-	return &Patch{
-		plantType:  plantType,
-		areas:      make([]int, 0),
-		overlaps:   make([]int, 0),
-		borderSets: make([]mapset.Set[Coordinate], 0),
+type Direction int
+
+const (
+	Root Direction = iota
+	Right
+	Down
+	Left
+	Up
+)
+
+func NewRegion(plantType string) *Region {
+	return &Region{
+		plantType: plantType,
+		areas:     make([]int, 0),
+		overlaps:  make([]int, 0),
+		patches:   make([]mapset.Set[Coordinate], 0),
 	}
 }
 
-/*func (p *Patch) updateBordersv2() {
-	thisY, nextY := p.points[len(p.points)-2].Y, p.points[len(p.points)-1].Y
-	thisX, nextX := p.points[len(p.points)-2].X, p.points[len(p.points)-1].X
-
-	if nextY == thisY && nextX == thisX+1 && !slices.Contains(p.points, utils.Point{X: nextX, Y: nextY - 1}) {
-		p.borderMap["N"]++
-		p.borderMap["S"]++
-		p.perimeter += 2
-
-	} else if nextY == thisY+1 {
-		if slices.Contains(p.points, utils.Point{X: nextX, Y: nextY - 1}) {
-			p.borderMap["W"]++
-			p.borderMap["E"]++
-			p.perimeter += 2
-
-		} else {
-			p.borderMap["N"]++
-			p.borderMap["W"]++
-			p.borderMap["E"]++
-			p.perimeter += 3
-
-		}
-	} else if !slices.Contains(p.points, utils.Point{Y: nextY, X: nextX - 1}) &&
-		!slices.Contains(p.points, utils.Point{Y: nextY - 1, X: nextX}) {
-		p.borderMap["N"]++
-		p.borderMap["W"]++
-		p.borderMap["E"]++
-		p.borderMap["S"]++
-		p.perimeter += 4
+func (p *Region) calcPerimeters() {
+	p.perimeters = make([]int, len(p.areas))
+	for i, area := range p.areas {
+		p.perimeters[i] = area * (area*4 - (p.overlaps[i] * 2))
 	}
-}*/
+}
 
-/*func (p *Patch) updateBorders() {
-	for _, coord := range p.coords {
-		thisY, nextY := coord[len(p.coords)-2].Y, coord[len(p.coords)-1].Y
-		thisX, nextX := coord[len(p.coords)-2].X, coord[len(p.coords)-1].X
+func (p *Region) findPatchRec(grid []string, char string, setIdx int, yPos, xPos int, dir Direction) {
+	p.areas[setIdx]++
+	p.patches[setIdx].Add(Coordinate{idx: setIdx, pnt: utils.Point{Y: yPos, X: xPos}})
+	leftCd := Coordinate{idx: setIdx, pnt: utils.Point{Y: yPos, X: xPos - 1}}
+	rightCd := Coordinate{idx: setIdx, pnt: utils.Point{Y: yPos, X: xPos + 1}}
+	downCd := Coordinate{idx: setIdx, pnt: utils.Point{Y: yPos + 1, X: xPos}}
+	upCd := Coordinate{idx: setIdx, pnt: utils.Point{Y: yPos - 1, X: xPos}}
 
-		if nextY == thisY && nextX == thisX+1 && !slices.Contains(coord, utils.Point{X: nextX, Y: nextY - 1}) {
-			p.borderMap["N"]++
-			p.borderMap["S"]++
-			p.perimeter += 2
+	if !p.patches[setIdx].Contains(rightCd) {
+		if xPos < len(grid[0])-1 {
+			if string(grid[yPos][xPos+1]) == char {
+				p.overlaps[setIdx]++
+				p.findPatchRec(grid, char, setIdx, yPos, xPos+1, Right)
+			}
+		}
+	} else if !(dir == Left) {
+		p.overlaps[setIdx]++
+	}
 
-		} else if nextY == thisY+1 {
-			if slices.Contains(coord, utils.Point{X: nextX, Y: nextY - 1}) {
-				p.borderMap["W"]++
-				p.borderMap["E"]++
-				p.perimeter += 2
-			} else {
-				p.borderMap["N"]++
-				p.borderMap["W"]++
-				p.borderMap["E"]++
-				p.perimeter += 3
+	if !p.patches[setIdx].Contains(downCd) {
+		if yPos < len(grid)-1 {
+			if string(grid[yPos+1][xPos]) == char {
+				p.overlaps[setIdx]++
+				p.findPatchRec(grid, char, setIdx, yPos+1, xPos, Down)
+			}
+		}
+	} else if !(dir == Up) {
+		p.overlaps[setIdx]++
+	}
+
+	if !p.patches[setIdx].Contains(leftCd) {
+		if xPos > 0 {
+			if string(grid[yPos][xPos-1]) == char {
+				p.overlaps[setIdx]++
+				p.findPatchRec(grid, char, setIdx, yPos, xPos-1, Left)
 			}
 		}
 	}
-}
-*/
 
-/*func (p *Patch) calcBorders() {
-	tempList := make([]utils.Point, 0)
-	for i, pnt := range p.points {
-		if !slices.Contains(tempList, pnt) {
-			tempList = append(tempList, pnt)
+	if !p.patches[setIdx].Contains(upCd) {
+		if yPos > 0 {
+			if string(grid[yPos-1][xPos]) == char {
+				p.overlaps[setIdx]++
+				p.findPatchRec(grid, char, setIdx, yPos-1, xPos, Up)
+			}
 		}
-		for j := i + 1; j < len(p.points); j++ {
-			if pnt.X == p.points[j].X || pnt.Y == p.points[j].Y {
-				if !slices.Contains(tempList, p.points[j]) {
-					tempList = append(tempList, p.points[j])
+	}
+
+	return
+}
+
+func parseRegions(input []string) map[string]*Region {
+	regionMap := make(map[string]*Region)
+	for y, row := range input {
+		for x, elem := range row {
+			char := string(elem)
+			if patch, ok := regionMap[char]; ok {
+				isIn := false
+				// check if plant is already in a patch
+				for i, elem := range patch.patches {
+					thisCd := Coordinate{idx: i, pnt: utils.Point{Y: y, X: x}}
+					if elem.Contains(thisCd) {
+						isIn = true
+						break
+					}
 				}
+
+				if !isIn {
+					patch.patches = append(patch.patches, mapset.NewSet[Coordinate]())
+					newIdx := len(patch.patches) - 1
+					patch.areas = append(patch.areas, 0)
+					patch.overlaps = append(patch.overlaps, 0)
+					patch.findPatchRec(input, char, newIdx, y, x, Root)
+				}
+			} else {
+				regionMap[char] = NewRegion(char)
+				p := regionMap[char]
+				p.patches = append(p.patches, mapset.NewSet[Coordinate]())
+				p.areas = append(p.areas, 0)
+				p.overlaps = append(p.overlaps, 0)
+				p.findPatchRec(input, char, 0, y, x, Root)
+
 			}
 		}
-
 	}
-}*/
+	return regionMap
+}
 
-func calcCost(garden map[string]*Patch) int {
+func calcCost(garden map[string]*Region) int {
 	totalCost := 0
 	for _, patch := range garden {
 		patch.calcPerimeters()
@@ -115,167 +145,8 @@ func calcCost(garden map[string]*Patch) int {
 	return totalCost
 }
 
-func (p *Patch) calcPerimeters() {
-	p.perimeters = make([]int, len(p.areas))
-	for i, area := range p.areas {
-		p.perimeters[i] = area * (area*4 - (p.overlaps[i] * 2))
-	}
-}
-
-func parsePatches(input []string) map[string]*Patch {
-	regionMap := make(map[string]*Patch)
-	for y, row := range input {
-		for x, elem := range row {
-			char := string(elem)
-			if key, ok := regionMap[char]; ok {
-				isIn := false
-				for i, elem := range key.borderSets {
-					thisPnt := utils.Point{Y: y, X: x}
-					upCoord := Coordinate{idx: i, pnt: utils.Point{Y: y - 1, X: x}}
-					backCoord := Coordinate{idx: i, pnt: utils.Point{Y: y, X: x - 1}}
-					forwCoord := Coordinate{idx: i, pnt: utils.Point{Y: y, X: x + 1}}
-
-					if elem.Contains(upCoord) || elem.Contains(backCoord) ||
-						elem.Contains(forwCoord) {
-						elem.Add(Coordinate{idx: i, pnt: thisPnt})
-						key.areas[i]++
-						// update overlap
-						if x <= len(row)-2 {
-							if string(row[x+1]) == char {
-								elem.Add(forwCoord)
-								key.overlaps[i]++
-							}
-						}
-						if y <= len(input)-2 {
-							if string(input[y+1][x]) == char {
-								elem.Add(Coordinate{idx: i, pnt: utils.Point{Y: y + 1, X: x}})
-								if tempX := x - 1; tempX > 0 {
-									for string(input[y+1][tempX]) == char {
-										elem.Add(Coordinate{idx: i, pnt: utils.Point{Y: y + 1, X: tempX}})
-										tempX--
-										if tempX < 0 {
-											break
-										}
-									}
-								}
-								key.overlaps[i]++
-							}
-						}
-						isIn = true
-					}
-				}
-				if !isIn {
-					key.borderSets = append(key.borderSets, mapset.NewSet[Coordinate]())
-					newIdx := len(key.borderSets) - 1
-					key.borderSets[newIdx].Add(Coordinate{idx: newIdx, pnt: utils.Point{Y: y, X: x}})
-					key.areas = append(key.areas, 1)
-					key.overlaps = append(key.overlaps, 0)
-					if x <= len(row)-2 {
-						if string(row[x+1]) == char {
-							key.borderSets[newIdx].Add(Coordinate{idx: 0, pnt: utils.Point{Y: y, X: x + 1}})
-							key.overlaps[newIdx]++
-						}
-					}
-					if y <= len(input)-2 {
-						if string(input[y+1][x]) == char {
-							key.borderSets[newIdx].Add(Coordinate{idx: 0, pnt: utils.Point{Y: y + 1, X: x}})
-							key.overlaps[newIdx]++
-						}
-					}
-
-				}
-				//key.points = append(key.points, utils.Point{Y: y, X: x})
-				//key.areas++
-
-			} else {
-				regionMap[char] = NewPatch(char)
-				p := regionMap[char]
-				p.borderSets = append(p.borderSets, mapset.NewSet[Coordinate]())
-				p.borderSets[0].Add(Coordinate{idx: 0, pnt: utils.Point{Y: y, X: x}})
-				p.areas = append(p.areas, 1)
-				p.overlaps = append(p.overlaps, 0)
-				// update overlap
-				if x <= len(row)-2 {
-					if string(row[x+1]) == char {
-						p.borderSets[0].Add(Coordinate{idx: 0, pnt: utils.Point{Y: y, X: x + 1}})
-						p.overlaps[0]++
-					}
-				}
-				if y <= len(input)-2 {
-					if string(input[y+1][x]) == char {
-						p.borderSets[0].Add(Coordinate{idx: 0, pnt: utils.Point{Y: y + 1, X: x}})
-						p.overlaps[0]++
-					}
-				}
-				//p.points = append(p.points, utils.Point{Y: y, X: x})
-				//p.areas++
-			}
-		}
-	}
-	return regionMap
-}
-
-func (p *Patch) findPathRec(grid []string, char string, setIdx int, yPos, xPos int) {
-	p.areas[setIdx]++
-	p.borderSets[setIdx].Add(Coordinate{idx: setIdx, pnt: utils.Point{Y: yPos, X: xPos}})
-	rightCd := Coordinate{idx: setIdx, pnt: utils.Point{Y: yPos, X: xPos + 1}}
-	downCd := Coordinate{idx: setIdx, pnt: utils.Point{Y: yPos + 1, X: xPos}}
-
-	if !p.borderSets[setIdx].Contains(rightCd) {
-		if string(grid[yPos][xPos+1]) == char {
-			p.overlaps[setIdx]++
-			p.findPathRec(grid, char, setIdx, yPos, xPos+1)
-		}
-	} else {
-		p.overlaps[setIdx]++
-	}
-
-	if !p.borderSets[setIdx].Contains(downCd) {
-		if string(grid[yPos+1][xPos]) == char {
-			p.overlaps[setIdx]++
-			p.findPathRec(grid, char, setIdx, yPos+1, xPos)
-		}
-	} else {
-		p.overlaps[setIdx]++
-	}
-	return
-}
-
-func parsePatchesV2(input []string) map[string]*Patch {
-	regionMap := make(map[string]*Patch)
-	for y, row := range input {
-		for x, elem := range row {
-			char := string(elem)
-			if patch, ok := regionMap[char]; ok {
-				isIn := false
-
-				for i := range patch.borderSets {
-					patch.findPathRec(input, char, i, y, x)
-				}
-
-				if !isIn {
-
-				}
-			} else {
-				regionMap[char] = NewPatch(char)
-				p := regionMap[char]
-				p.borderSets = append(p.borderSets, mapset.NewSet[Coordinate]())
-				p.areas = append(p.areas, 0)
-				p.overlaps = append(p.overlaps, 0)
-				p.findPathRec(input, char, 0, y, x)
-				fmt.Println("per: ", p.areas[0]*(p.areas[0]*4-p.overlaps[0]*2))
-				fmt.Println("finished")
-			}
-		}
-	}
-	return regionMap
-}
-
 func PrintPart1() {
-	input, _ := utils.ReadLines("day12/test0.txt")
-	result := parsePatchesV2(input)
+	input, _ := utils.ReadLines("day12/input.txt")
+	result := parseRegions(input)
 	fmt.Println("AoC 24 Day 12, Part 1:", calcCost(result))
-	for k, p := range result {
-		fmt.Printf("%v: %v\n", k, p)
-	}
 }
