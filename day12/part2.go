40f8e8972e8eaefc1606187dac9c7239a3b861fc
FaceOfBo0 <70140104+FaceOfBo0@users.noreply.github.com>
2025-02-28 15:38:09 +0100

    n.i.

--- day12/part2.go
+++ day12/part2.go
@@ -1,6 +1,12 @@
 package day12
 
+import (
+	"AoC_24/utils"
+	"fmt"
+)
+
 func PrintPart2() {
-	//input, _ := utils.ReadLines("day12/input.txt")
-	// fmt.Println("AoC 24 Day 12, Part 2:", 0)
+	input, _ := utils.ReadLines("day12/input.txt")
+	result := parseRegions(input)
+	fmt.Println("AoC 24 Day 12, Part 2:", result)
 }
